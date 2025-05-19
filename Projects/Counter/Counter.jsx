57cf7fe0838bf5d29f9276d3b37a20863cfe51c2
Harshad <harshadadagale05@gmail.com>
2025-05-19 14:19:35 +0530

    Improved Counter Project

--- Projects/Counter/Counter.jsx
+++ Projects/Counter/Counter.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import './style.css'
+import './style.css';
 
 const Counter = () => {
 
@@ -9,14 +9,17 @@ const Counter = () => {
 
     const decrement = () => setCount(count - 1);
 
+    const reset = () => setCount(0);
+
     return <div className="container">
                  <div>
                  <h1 className="number">{count}</h1>
                  </div>
 
                 <div className="btns-container ">
-                    <button className="increment" onClick={increment} >+</button>
                     <button className="increment" onClick={decrement} >-</button>
+                    <button className="increment" onClick={reset} >Reset</button>
+                    <button className="increment" onClick={increment} >+</button>
                 </div>
             </div>
 }
