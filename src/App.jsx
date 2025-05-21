976ea7f9c8c7cad8b8f67ebc94b40a5333605056
Harshad <harshadadagale05@gmail.com>
2025-05-21 15:56:26 +0530

    Project 4 Completed - Basic Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import Counter from '../Projects/Counter/Counter.jsx'
 // import Todo from "../Projects/Todo/Todo.jsx";
-import Meal from '../Projects/Meals/Meal.jsx';
+// import Meal from '../Projects/Meals/Meal.jsx';
+// import Todo from '../Projects/Todo/Todo.jsx'
+// import Meal from "../Projects/Meals/Meal.jsx"
+import Calculator from "../Projects/Calculator/Calculator.jsx";
 
 function App() {
     return <>
             {/*<Counter/>*/}
             {/*< Todo />*/}
-            < Meal />
+            {/*< Meal />*/}
+            <Calculator />
             </>
 }
 
